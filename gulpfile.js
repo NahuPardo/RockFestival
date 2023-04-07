017fbb390db5f036e8e8184ccc9aa1828fe3d7c3
Nahuel <Nahuelpardo3@gmail.com>
2023-04-07 17:03:48 -0300

    cambiando formato de imagenes a avif con gulp

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,10 @@ const plumber = require('gulp-plumber');
 
 
 // Imagenes
+const cache = require('gulp-cache')
+const imagemin = require('gulp-imagemin')
 const webp = require('gulp-webp');
+const avif = require('gulp-avif');
 
 function css (done) {
 
@@ -18,18 +21,40 @@ function css (done) {
     done(); // Callback que avisa a gulp cuando llegamos al final
 }
 
+function imagenes(done) {
+    const opciones = {
+        optimizationLevel: 3
+    }
+
+    src('src/img/**/*')
+    .pipe( cache( imagemin(opciones) ))
+    .pipe( dest('build/img'))
+
+    done();
+}
+
 function versionWebp (done) {   
 
     const opciones = {
         quality: 50
     };
-
     src('src/img/**/*')
     .pipe( webp(opciones) )
     .pipe( dest('build/img'));
-
     done();
 }
+
+function versionAvif (done) {   
+
+    const opciones = {
+        quality: 50
+    };
+    src('src/img/**/*.jpg')
+    .pipe( avif(opciones) )
+    .pipe( dest('build/img'));
+    done();
+}
+
 
 function dev(done){
     watch('src/scss/**/*.scss', css);
@@ -39,5 +64,7 @@ function dev(done){
 }
 
 exports.css = css;
+exports.imagenes = imagenes;
 exports.versionWebp = versionWebp;
-exports.dev = parallel(versionWebp, dev);
+exports.versionAvif = versionAvif;
+exports.dev = parallel(imagenes, versionWebp, versionAvif, dev);
