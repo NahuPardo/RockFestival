49b081f9218c567bd976c4b9ca5fd6f3ca7657b2
Nahuel <Nahuelpardo3@gmail.com>
2023-04-14 14:57:30 -0300

    creando boletos

--- src/js/galeria.js
+++ src/js/galeria.js
@@ -16,7 +16,45 @@ function crearGaleria () {
             <source srcset="build/img/thumb/${i}.webp" type="img/webp">
             <img loading="lazy" width="200" height="300" src="build/img/thumb/${i}.jpg" alt="Imagen galeria">
             `;
+            imagen.onclick = function () {
+                mostrarImagen(i);
+            }
 
-        galeria.appendChild(imagen);
+            galeria.appendChild(imagen);
     }
+}
+
+function mostrarImagen(id) {
+    const imagen = document.createElement('picture');
+    imagen.innerHTML = `
+        <source srcset="build/img/grande/${id}.avif" type="img/avif">
+        <source srcset="build/img/grande/${id}.webp" type="img/webp">
+        <img loading="lazy" width="200" height="300" src="build/img/grande/${id}.jpg" alt="Imagen galeria">
+        `;
+
+        // Crea el overlay con la imagen
+        const overlay = document.createElement('DIV');
+        overlay.appendChild(imagen);
+        overlay.classList.add('overlay');
+        overlay.onclick = function () {
+            const body = document.querySelector('body')
+            body.classList.remove('fijar-body');
+            overlay.remove();
+        }
+
+        // Boton para cerrar el modal
+        const cerrarModal = document.createElement('P');
+        cerrarModal.textContent = 'X';
+        cerrarModal.classList.add('btn-cerrar');
+        cerrarModal.onclick = function (){
+            const body = document.querySelector('body')
+            body.classList.remove('fijar-body');
+            overlay.remove();
+        }
+        overlay.appendChild(cerrarModal);
+
+        // Lo agrega al html
+        const body = document.querySelector('body')
+        body.appendChild(overlay);
+        body.classList.add('fijar-body');
 }
